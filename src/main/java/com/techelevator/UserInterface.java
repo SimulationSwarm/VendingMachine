c4482a2be763c1e2fdb8b5de2c4e62aa97c7a2fd
Jared Hance <jarhance@gmail.com>
2023-02-10 11:53:54 -0500

    Fixed user interface methods

--- src/main/java/com/techelevator/UserInterface.java
+++ src/main/java/com/techelevator/UserInterface.java
@@ -18,11 +18,9 @@ public class UserInterface {
         mainMenu();
     }
 
-
     public void mainMenu() throws FileNotFoundException {
-
-        String menuSelection;
-        do {
+        String menuSelection = "";
+        while (!menuSelection.equals("3")) {
             System.out.println("(1) Display Vending Machine Items");
             System.out.println("(2) Purchase");
             System.out.println("(3) Exit");
@@ -31,59 +29,52 @@ public class UserInterface {
                 vendingMachine.inventory.displayInventory();
             } else if (menuSelection.equals("2")) {
                 purchaseMenu();
+
             } else if (menuSelection.equals("3")) {
                 System.out.println("Goodbye!");
                 System.exit(1);
             } else {
                 System.out.println("Invalid input, try again.");
             }
-       } while (!menuSelection.equals("3"));
-
+        }
     }
 
     public void purchaseMenu() throws FileNotFoundException {
-        System.out.println("(1) Feed Money");
-        System.out.println("(2) Select Product");
-        System.out.println("(3) Finish Transaction");
-        String menuSelection = userInput.nextLine();
-        while (!menuSelection.equals("3")) {
-            if (menuSelection.equals("1")) {
+        String purchaseMenuSelection = "";
+        while (!purchaseMenuSelection.equals("3")) {
+            System.out.println("(1) Feed Money");
+            System.out.println("(2) Select Product");
+            System.out.println("(3) Finish Transaction");
+            purchaseMenuSelection = userInput.nextLine();
+            if (purchaseMenuSelection.equals("1")) {
                 feedMoney();
-                } else if (menuSelection.equals("2")) {
+                } else if (purchaseMenuSelection.equals("2")) {
                 purchaseProductMenu();
             }
         }
         vendingMachine.giveBackChange();
         vendingMachine.setCurrentMoney(0.0);
-        mainMenu();
-
-
     }
 
     public void feedMoney() throws FileNotFoundException {
-        System.out.println("(1) Add Money");
-        System.out.println("(2) Current Balance");
-        System.out.println("(3) Return to Previous Menu");
-        String menuSelection = userInput.nextLine();
-        while (!menuSelection.equals("3")) {
-            if (menuSelection.equals("1")) {
+        String feedMoneyMenuSelection = "";
+        while (!feedMoneyMenuSelection.equals("3")) {
+            System.out.println("(1) Add Money");
+            System.out.println("(2) Current Balance");
+            System.out.println("(3) Return to Previous Menu");
+            feedMoneyMenuSelection = userInput.nextLine();
+            if (feedMoneyMenuSelection.equals("1")) {
                 System.out.println("How much would you like to add?");
                 int moneyToAdd = Integer.parseInt(userInput.nextLine());
-                while (moneyToAdd != 0) {
                     if (moneyToAdd < 0) {
                         System.out.println("Invalid amount.");
-                        return;
                     } else {
                         vendingMachine.addMoney(moneyToAdd);
-                        return;
                     }
-                }
-            } else if (menuSelection.equals("2")) {
+            } else if (feedMoneyMenuSelection.equals("2")) {
                 System.out.println("Your current balance is " + vendingMachine.getCurrentMoney() + ".");
-                return;
             }
         }
-        purchaseMenu();
     }
 
     public void purchaseProductMenu() throws FileNotFoundException {
@@ -91,15 +82,11 @@ public class UserInterface {
         String itemPurchased = userInput.nextLine();
         if (!vendingMachine.inventory.checkInventory(itemPurchased)) {
             System.out.println("Selected item is out of stock.");
-            purchaseMenu();
+
         }
         if (!vendingMachine.checkMoney(itemPurchased)) {
             System.out.println("Insufficient funds.");
-            purchaseMenu();
         }
         vendingMachine.makePurchase(itemPurchased);
-        purchaseMenu();
-
     }
-
 }
